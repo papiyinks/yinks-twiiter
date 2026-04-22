6cdf0af5afcca276c5300f474b7e6dcc235f5bda
Yinka Adeniyi <yinka.adeniyi@lumiqualis.com>
2026-04-22 13:37:43 -0400

    dynamic routes added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
 import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,11 @@ const router = createBrowserRouter([
         loader: postsLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: newPostAction },
+          {
+            path: "/:id",
+            element: <PostDetails />,
+            loader: postDetailsLoader,
+          },
         ],
       },
     ],
